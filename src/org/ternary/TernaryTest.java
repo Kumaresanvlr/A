73680b07860499a8e16837855c3720a789273843
kumaresanvlr <kumaresanvlr@gmail.com>
2022-05-14 16:47:40 +0530

    Psuh Ternary

---
+++ src/org/ternary/TernaryTest.java
@@ -0,0 +1,13 @@
+package org.ternary;
+
+public class TernaryTest {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 5;
+		int greaterNum = (a==b)?b:a;
+		System.out.println(greaterNum);
+
+	}
+
+}
